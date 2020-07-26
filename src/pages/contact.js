586f6cc49d5b33ff15288b67a9fa3f6d96ada206
virensuthar <51020740+virensuthar@users.noreply.github.com>
2020-07-26 11:09:33 +0530

    contact section

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/Reuseable/HeroSection"
+import Infoblock from "../components/Reuseable/Infoblock"
+import Contact from "../components/contact/Contact"
 
-export default function contact() {
-  return (
-    <div>
-      <h1>this is contact page</h1>
-    </div>
-  )
-}
+const ContactPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="Contact Us"
+      subtitle=""
+      heroclass="about-background "
+    />
+
+    <Infoblock heading="How can we help?" />
+    <Contact />
+  </Layout>
+)
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "contact.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default ContactPage
